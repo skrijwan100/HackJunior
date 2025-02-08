d51064bf3d2dcdcd3278503175c6558464b95d2d
skrijwan <skrijwan329@gmail.com>
2025-02-09 00:15:58 +0530

    add all file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+
+
+  )
+}
+
+export default App
+
