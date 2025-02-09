be4bbbc192a6b7e15f4a852397f19f3e8d8dd807
skrijwan <skrijwan329@gmail.com>
2025-02-10 00:52:21 +0530

    add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import {
+  BrowserRouter ,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import Home from './components/Home'
+import Register from './components/Register';
 
 function App() {
 
   return (
     <>
-      <Home />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+      
     </>
 
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import "../App.css"
+import "../media.css"
 // import Navbar from './components/Navbar'
 import logo from "../assets/Hackjunior logo.png"
+import Footar from './Footar'
 export default function Home() {
     return (
         <>
             <nav>
-                <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#0a1029", alignItems: "center" }}>
-                    <div style={{ width: "90px", height: "90px" }}>
+                {/* <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#0a1029", alignItems: "center",height: "90px" }}>
+                    <div className='logopic' style={{ width: "90px", height: "90px" }}>
 
                         <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} src={logo} alt="This is logo" />
                     </div>
                     <div className="allnavigation">
-                        <ul style={{ display: "flex", gap: "28px", listStyle: "none", fontSize: "25px", fontFamily: "Jost", color: "white", marginRight: "19px" }}>
+                        <ul style={{ display: "flex",justifyContent:"space-between", listStyle: "none", fontSize: "25px", fontFamily: "Jost", color: "white", marginRight: "19px" }}>
+                            <li className='navlink' style={{ cursor: "pointer" }}>Home</li>
+                            <li className='navlink' style={{ cursor: "pointer" }}>About</li>
+                            <li className='navlink' style={{ cursor: "pointer" }}>Q.&.A</li>
+                            <li className='navlink' style={{ cursor: "pointer" }}>Register</li>
+                        </ul>
+                    </div>
+                </div> */}
+                <div className='mainNavBox'>
+                    <div className='logopic' style={{ width: "90px", height: "90px" }}>
+
+                        <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} src={logo} alt="This is logo" />
+                    </div>
+                    <div className="allnavigation">
+                        <ul style={{ display: "flex",justifyContent:"space-between", listStyle: "none", fontSize: "25px", fontFamily: "Tektur", color: "white", marginRight: "19px" }}>
                             <li className='navlink' style={{ cursor: "pointer" }}>Home</li>
                             <li className='navlink' style={{ cursor: "pointer" }}>About</li>
                             <li className='navlink' style={{ cursor: "pointer" }}>Q.&.A</li>
@@ -21,18 +37,21 @@ export default function Home() {
                     </div>
                 </div>
 
+
             </nav>
 
             <div id='home'>
-                <header style={{ fontFamily: "Bebas Neue", fontSize: "100px", fontWeight: "400", textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-                    <p>HACKJUNIOR</p>
+                <header className='headertext' style={{ fontSize: "70px", fontWeight: "400", textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" ,textShadow:"3px 9px 6px #000000db"}}>
+                    <p style={{ fontFamily: "Tektur", color: "#0093ff" }}>HACKJUNIOR</p>
                     <div style={{ width: "150px", height: "140px" }}>
 
                         <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} src={logo} alt="This is logo" />
                     </div>
                 </header>
 
+
             </div>
+            <Footar/>
         </>
     )
 }
