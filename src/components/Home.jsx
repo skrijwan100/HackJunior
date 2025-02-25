2d8fdf3a1b33b553d3f4bacc4ce022a2376c61d6
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-02-25 13:47:33 +0530

    add tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 import "../App.css"
 // import Navbar from './components/Navbar'
-import logo from "../assets/Hackjunior logo.png"
+import logo from "../assets/Hackjunior_logo.png"
+import Navbar from './Navbar'
 export default function Home() {
     return (
         <>
-            <nav>
-                <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#0a1029", alignItems: "center" }}>
-                    <div style={{ width: "90px", height: "90px" }}>
-
-                        <img style={{ width: "100%", height: "100%", borderRadius: "50%" }} src={logo} alt="This is logo" />
-                    </div>
-                    <div className="allnavigation">
-                        <ul style={{ display: "flex", gap: "28px", listStyle: "none", fontSize: "25px", fontFamily: "Jost", color: "white", marginRight: "19px" }}>
-                            <li className='navlink' style={{ cursor: "pointer" }}>Home</li>
-                            <li className='navlink' style={{ cursor: "pointer" }}>About</li>
-                            <li className='navlink' style={{ cursor: "pointer" }}>Q.&.A</li>
-                            <li className='navlink' style={{ cursor: "pointer" }}>Register</li>
-                        </ul>
-                    </div>
-                </div>
-
-            </nav>
+            <Navbar />
 
             <div id='home'>
                 <header style={{ fontFamily: "Bebas Neue", fontSize: "100px", fontWeight: "400", textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
