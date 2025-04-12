ccb0e77e6c29aeaefa1dadfe0b4c1476b8ed4c25
skrijwan <skrijwan329@gmail.com>
2025-04-13 02:50:13 +0530

    add new background

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import {
-  BrowserRouter ,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { cn } from "./lib/utlis";
+import React from "react";
 
-import Home from './components/Home'
-import Register from './components/Register';
+import './App.css'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/register' element={<Register/>}/>
-    </Routes>
-    </BrowserRouter>
-      
+      <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+        <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+         
+        </p>
+      </div>
+
     </>
 
 
   )
 }
 
-export default App
+export default App;
 
